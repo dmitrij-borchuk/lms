45c61a7e427095704942f129299ba0cfe782a159
dmitrij-borchuk <frunk.lern@gmail.com>
2017-03-26 12:51:36 -0700

    Removed ember

--- app/adapters/application.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
-
-const { JSONAPIAdapter } = DS;
-
-export default JSONAPIAdapter.extend(DataAdapterMixin, {
-  authorizer: 'authorizer:application'
-});
